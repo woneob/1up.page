41fd3675990bfd21742c5989d32c476f408b3def
1UP <1up@1upz.com>
2025-12-18 00:58:49 +0900

    사이트 설정을 yaml 파일로 교체

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import site from '~/data/site.config.json';
+import site from '~/data/site.config.yml';
 
 
 export async function GET(context) {
