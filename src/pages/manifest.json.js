3fc2d06955430840ed1aa034a24ec986c83b3534
1UP <1up@1upz.com>
2025-12-24 02:46:45 +0900

    사이트 매니페스트 설정 추가
    사이트 아이콘의 크기 및 용도 속성 추가
    매니페스트 파일을 생성하여 사이트 정보 추가

---
+++ src/pages/manifest.json.js
@@ -0,0 +1,27 @@
+import site from '~/data/site.config.yml';
+
+const data = {
+  short_name: site.siteName,
+  name: site.siteName,
+  id: site.siteName,
+  description: site.tagline,
+  orientation: site.orientation,
+  start_url: site.startUrl,
+  display: site.display,
+  background_color: site.backgroundColor,
+  theme_color: site.themeColor,
+  icons: (site.siteIcons || []).map(({ href, type, sizes, purpose }) => ({
+    src: href,
+    type,
+    sizes,
+    purpose
+  }))
+};
+
+export async function GET() {
+  return new Response(JSON.stringify(data, null, 2), {
+    headers: {
+      'Content-Type': 'application/manifest+json; charset=utf-8', // 매니페스트 전용 MIME 타입 권장
+    },
+  });
+}
